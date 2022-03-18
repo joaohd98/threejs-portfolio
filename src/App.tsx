886aa0a50336b0932b6177c17058162240af6b77
João Damazio <joao.damazio@fiap.com.br>
2022-03-18 00:42:16 -0300

    Add three with example project

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,50 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import * as three  from 'three'
+import {Canvas, useFrame} from '@react-three/fiber'
+import {FC, useRef} from "react";
+import {BoxHelper, PointLightHelper, GridHelper} from "three";
+import {OrbitControls, useHelper} from "@react-three/drei";
 
-function App() {
-  const [count, setCount] = useState(0)
+const camera = new three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.setZ(30);
+
+const MeshTouros: FC = () => {
+  const mesh = useRef<three.Mesh>();
+  const pointLight = useRef<three.PointLight>();
+
+  useFrame(() => {
+    if(mesh.current) {
+      mesh.current.rotation.x += 0.01;
+      mesh.current.rotation.y += 0.005;
+      mesh.current.rotation.z += 0.01;
+    }
+  });
+
+  useHelper(pointLight, PointLightHelper)
 
   return (
+    <mesh ref={mesh}>
+      <torusGeometry args={[10, 3, 16, 100]} />
+      <pointLight ref={pointLight} color={0xffffff} position={[10, 10, 10]} />
+      <ambientLight color={0xffffff} />
+      <meshStandardMaterial color={0xFF6347}  />
+    </mesh>
+  )
+}
+
+function App() {
+  return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <div className="canvas-container">
+        <Canvas camera={camera} >
+          <MeshTouros />
+          <OrbitControls camera={camera} makeDefault />
+          <gridHelper args={[200, 50]} />
+        </Canvas>
+      </div>
     </div>
   )
 }
 
 export default App
+
